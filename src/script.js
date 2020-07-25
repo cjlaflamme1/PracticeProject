fca59f83b5ec4f0aa0ccb15963435beaec0069a5
BR3NDAN <br3ndan.l8n@gmail.com>
2020-07-25 12:40:07 -0400

    Wroking on JS

--- src/script.js
+++ src/script.js
@@ -1,5 +1,21 @@
 $('document').ready(function () {
+const searchTermInput = $('');
+const numRecordsRetrieve = $('');
 
+let searchTerm = 'election'
 
-    
+var queryURL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchTerm}&api-key=VGZwZNo5TbgkYdXmf6BZEsLOkiJ0Psae`;
+
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+          console.log(response);
+        //   response.docs[i]
+        var articleArray = $(response.docs);
+        articleArray.forEach(function (index) {
+            console.log(articleArray[index]);
+        });
+    });
+
 });
