dc33b84f0d9072b7c244ad1e9e12d12934688f4b
Dwayne Risley <risleydwayne@gmail.com>
2020-07-25 12:54:45 -0400

    article array declaration fix

--- src/script.js
+++ src/script.js
@@ -1,21 +1,25 @@
 $('document').ready(function () {
-const searchTermInput = $('');
-const numRecordsRetrieve = $('');
+  const searchTermInput = $('');
+  const numRecordsRetrieve = $('');
 
-let searchTerm = 'election'
+  let searchTerm = 'election'
 
-var queryURL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchTerm}&api-key=VGZwZNo5TbgkYdXmf6BZEsLOkiJ0Psae`;
+  var queryURL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchTerm}&api-key=VGZwZNo5TbgkYdXmf6BZEsLOkiJ0Psae`;
 
-      $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function(response) {
-          console.log(response);
-        //   response.docs[i]
-        var articleArray = $(response.docs);
-        articleArray.forEach(function (index) {
-            console.log(articleArray[index]);
-        });
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+
+    console.log(response);
+    //   response.docs[i]
+
+    var articleArray = $(response.response.docs);
+
+    $.each(articleArray,function(article){
+      console.log(articleArray[article]);
     });
+
+  });
 
 });
